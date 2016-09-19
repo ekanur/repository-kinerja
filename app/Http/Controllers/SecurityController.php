6e4b5ba5e36626ecbfecf7dda25c6416df3c1e21
Eka Nur Ahmad R <Eka Nur Ahmad R>
2016-09-19 13:50:54 +0700

    fix securitycontroller

--- app/Http/Controllers/SecurityController.php
+++ app/Http/Controllers/SecurityController.php
@@ -26,16 +26,14 @@ class SecurityController extends Controller {
                 $backToUrl = $_SESSION['JOSSO_ORIGINAL_URL'];
                 $josso_agent = \jossoagent::getNewInstance();
                 $ssoSessionId = $josso_agent->resolveAuthenticationAssertion($assertionId);
-<<<<<<< HEAD
+
                 // var_dump($ssoSessionId);exit();
                 if($ssoSessionId==null){
                     $ssoSessionId='D9980BE9B9893B8C5029BEEDDDA15B61';
                 }
                 
-=======
-                var_dump(Session::get('ketDosen'));
-                //$ssoSessionId='FD2A5F4ECE092613763619472061E373';
->>>>>>> 7ee81980ddd2f648e24f285056f8749829175d04
+
+               // var_dump(Session::get('ketDosen'));
                 // Set SSO Cookie ...
                 setcookie("JOSSO_SESSIONID", $ssoSessionId, 0, "/"); // session cookie ...
                 $_COOKIE['JOSSO_SESSIONID'] = $ssoSessionId;
